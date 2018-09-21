869536f7b776fb8b02d867c274dd67df56f2a11e
Xin Nie <donnanie08@gmail.com>
2018-09-21 10:11:13 -0400

    continue working on bag of words

--- proj01/xn9vc-bag-of-words2.py
+++ proj01/xn9vc-bag-of-words2.py
@@ -3,17 +3,18 @@ from nltk.corpus import stopwords
 # used for tokenize the words
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-# used for removing punctuations
-from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import wordpunct_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 
 class BagOfWords:
-    def clean_data(self, sentence):
+    def prepare_text(self, sentence):
         # data cleaning
 
-        # ignore punctuations & tokenize
-        regex_tokenizer = RegexpTokenizer(r'\w+')
-        cleaned_sentence = regex_tokenizer.tokenize(sentence)
+        # tokenize & filter all tokens that are not alphabetic
+        tokenized_sentence = word_tokenize(sentence)
+        cleaned_sentence = [w for w in tokenized_sentence if w.isalpha()]
+
 
         # convert all text to lower case
         cleaned_sentence = [s.lower() for s in cleaned_sentence]
@@ -29,32 +30,36 @@ class BagOfWords:
         stem_sentence = [ps.stem(w) for w in filter_sentence]
 
 
+
+
         return filter_sentence
 
 
-    def tokenize(self, sentences):
-        tokens = word_tokenize(sentences)
-        return tokens
-
 
     def bag_of_words(self):
+        # get a list of all sentences in the file
+        trn_data = open('./trn.data').read().strip().split('\n')
         trn_label = map(str, open('./trn.label').read().strip().split('\n'))
-        with open('./trn.data') as f:
-            trn_data = f.readlines()
-            trn_data = trn_data.strip().split('\n')
 
+        vecotrized = CountVectorizer(trn_data)
+
+        # build vocabulary
+        vocab = {'' : 0}
+        for data in trn_data:
+            cleaned_trn_data = wordpunct_tokenize(data.lower())
+            cleaned_trn_data = [w for w in cleaned_trn_data if w.isalpha()]
+            for w in cleaned_trn_data:
+                vocab[w] += 1
+            print(cleaned_trn_data)
 
         # trn_token = tokenize(trn_data[0])
         # clean_data(trn_token)
-        cleaned_trn_data = []
-        for sentence in trn_data:
-            print(self.clean_data(sentence))
-            # cleaned_trn_data.append(self.clean_data(sentence))
-
-        # print(cleaned_trn_data[1])
 
         return 0
 
 
 bag = BagOfWords()
 bag.bag_of_words()
+
+
+## Use merge sort to add tokens to vocabulary
